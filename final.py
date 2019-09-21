94b4dcf6b64fe5831869f8a86073715922f80645
Alicja Ciuńczyk <41646139+AlicjaCiunczyk@users.noreply.github.com>
2019-09-21 19:38:52 +0200

    logistic regression

--- final.py
+++ final.py
@@ -2,31 +2,28 @@
 import numpy as np
 import os
 from sklearn.preprocessing import StandardScaler
-# for tensorflow you first have to install it
-#import tensorflow as tf
-#import matplotlib.pyplot as plt
-#import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
 
 # fix random seed for reproducibility
 seed = 7
 np.random.seed(seed)
 
 ## loading the data
-written_train = np.load("written_train.npy", allow_pickle=True)
-written_test = np.load("written_test.npy", allow_pickle=True)
-spoken_train = np.load("spoken_train.npy", allow_pickle=True)
-spoken_test = np.load("spoken_test.npy", allow_pickle=True)
-match_train = np.load("match_train.npy", allow_pickle=True)
-
+written_tr = np.load("written_train.npy", allow_pickle=True)
+written_te = np.load("written_test.npy", allow_pickle=True)
+spoken_tr = np.load("spoken_train.npy", allow_pickle=True)
+spoken_te = np.load("spoken_test.npy", allow_pickle=True)
+match_tr = np.load("match_train.npy", allow_pickle=True)
 
 
 # subsetting the data to get some test set
-written_train_test = written_train[:4000,:]
-written_train_train = written_train[4000:,:]
-spoken_train_test = spoken_train[:4000]
-spoken_train_train = spoken_train[4000:]
-match_train_test = match_train[:4000] 
-match_train_train = match_train[4000:]
+written_test = written_tr[:4000,:]
+written_train = written_tr[4000:,:]
+spoken_test = spoken_tr[:4000]
+spoken_train = spoken_tr[4000:]
+match_test = match_tr[:4000] 
+match_train = match_tr[4000:]
 
 
 # fucntion to determine how many true/false values are in target
@@ -57,15 +54,13 @@ def feat_eng_spoken(spoken,summaries):
     return X_scaled
 
 #define functions to be applied to spoken data
-summaries=[np.mean, np.max, np.min, np.std, np.median, kurtosis, iqr, skew, np.sum, np.argmin, np.argmax]
+summaries=[np.mean, np.max, np.min, np.std, np.median, np.sum, np.argmin, np.argmax]
 
 
 # Preparing training data
 written_train=written_train.astype('float32')
 #normalise written
 written_train/=255
-#reshape it into 28 x 28 matrix
-written_train_m=written_train.reshape(written_train.shape[0],28,28,1)
 
 #apply functions on sproken
 spoken_train_m=feat_eng_spoken(spoken_train,summaries)
@@ -76,9 +71,15 @@ spoken_train_m=feat_eng_spoken(spoken_train,summaries)
 written_test=written_test.astype('float32')
 #normalise written
 written_test/=255
-#reshape it into 28 x 28 matrix
-written_test_m=written_test.reshape(written_test.shape[0],28,28,1)
 
 #apply functions on sproken
 spoken_test_m=feat_eng_spoken(spoken_test,summaries)
-print(spoken_test_m.shape)
+
+all_train = np.concatenate((written_train, spoken_train_m), axis = 1)
+all_test = np.concatenate((written_test, spoken_test_m), axis = 1)
+
+clf = LogisticRegression(random_state=0, solver='lbfgs')
+clf.fit(all_train, match_train)
+y_pred = clf.predict(all_test)
+
+accuracy_score(y_pred, match_test)
